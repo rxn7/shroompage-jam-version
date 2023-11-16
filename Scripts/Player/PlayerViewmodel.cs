7b13aa0f207bf952bdb02c311a8e050b097053c4
rxn7 <rotthin_dev@protonmail.com>
2023-11-17 00:54:57 +0100

    Customizable viewmodel animations per item

--- Scripts/Player/PlayerViewmodel.cs
+++ Scripts/Player/PlayerViewmodel.cs
@@ -11,7 +11,7 @@ internal partial class PlayerViewmodel : Node3D {
 	private const float SwayLerpSpeed = 10.0f;
 	private const float SwayResetMouseLastMovedTimerThreshold = 0.05f;
 	private const float HeadRollMultiplier = 3.0f;
-    private const float JumpFeedbackOffset = 0.002f;
+	private const float JumpFeedbackOffset = 0.002f;
 
 	public Vector3 Sway { get; private set; }
 	[Export] public PlayerViewmodelAnimationPlayer AnimPlayer { get; private set; }
