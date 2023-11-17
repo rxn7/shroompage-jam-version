886bb4d7276aaa8ca0373f2b3cdb584c663b549e
blackshibe <kacperszczepan@tutanota.com>
2023-11-18 00:54:19 +0100

    add orchids to credits

--- Scripts/SplashScreen/SplashScreenStage.cs
+++ Scripts/SplashScreen/SplashScreenStage.cs
@@ -3,6 +3,6 @@ using Godot;
 namespace Game.SplashScreen;
 
 internal partial class SplashScreenStage : Control {
-    [Export] public float FadeTime { get; private set; } = 0.25f ;
+	[Export] public float FadeTime { get; private set; } = 0.25f ;
 	[Export] public float DisplayTime { get; private set; }= 0.75f;
 }
